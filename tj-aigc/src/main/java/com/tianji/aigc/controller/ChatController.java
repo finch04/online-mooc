c045fe752fd970d961e41cffd911e1982b404c97
zhangzhijun <zhangzhijun@itcast.cn>
2025-01-16 11:48:47 +0800

    代码优化

--- tj-aigc/src/main/java/com/tianji/aigc/controller/ChatController.java
+++ tj-aigc/src/main/java/com/tianji/aigc/controller/ChatController.java
@@ -3,12 +3,14 @@ package com.tianji.aigc.controller;
 import com.tianji.aigc.service.ChatService;
 import com.tianji.common.annotations.NoWrapper;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @RestController
 public class ChatController {
 
@@ -18,6 +20,7 @@ public class ChatController {
     @NoWrapper // 自定义注解，记过不进行包装
     @PostMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> chat(@RequestParam("q") String question, @RequestParam("sessionId") String sessionId) {
+        log.info("+++++++++++" + question+"   ++++ " + sessionId);
         return this.chatService.chat(question, sessionId);
     }
 }
