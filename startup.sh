52f9331a5cca05e0e27e9aeaa6ac9e1b04a0ee4d
zhangzhijun <zhangzhijun@itcast.cn>
2025-01-18 10:35:00 +0800

    代码优化

--- startup.sh
+++ startup.sh
@@ -59,7 +59,7 @@ if [ "$DEBUG_PORT" = "0" ]; then
    -p "${PORT}:${PORT}" \
    -e JAVA_OPTS="${JAVA_OPTS}" \
    -e SPRING_PROFILES_ACTIVE=test \
-   --network heima-net ${IMAGE_NAME} \
+   ${IMAGE_NAME} \
   || exit 1
 else
   echo "run in debug mode"
@@ -68,7 +68,7 @@ else
    -p ${DEBUG_PORT}:5005 \
    -e JAVA_OPTS="${JAVA_OPTS}" \
    -e SPRING_PROFILES_ACTIVE=test \
-   --network heima-net ${IMAGE_NAME} \
+   ${IMAGE_NAME} \
   || exit 1
 fi
 echo "container is running now !! ^_^"
