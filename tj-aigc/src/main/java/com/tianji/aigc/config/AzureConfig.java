126063cc059f0e33c9b6a72f5da8425759739433
zhangzhijun <zhangzhijun@itcast.cn>
2025-03-04 19:52:03 +0800

    实现语音文字互转

--- tj-aigc/src/main/java/com/tianji/aigc/config/AzureConfig.java
+++ tj-aigc/src/main/java/com/tianji/aigc/config/AzureConfig.java
@@ -2,10 +2,12 @@ package com.tianji.aigc.config;
 
 import com.microsoft.cognitiveservices.speech.SpeechConfig;
 import com.microsoft.cognitiveservices.speech.SpeechSynthesisOutputFormat;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class AzureConfig {
 
@@ -18,6 +20,7 @@ public class AzureConfig {
 
     @Bean
     public SpeechConfig speechConfig() {
+        log.info("azure speech config init... region = {}, language = {}", region, language);
         SpeechConfig speechConfig = SpeechConfig.fromSubscription(subscriptionKey, region);
         speechConfig.setSpeechRecognitionLanguage(language);
         speechConfig.setSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3);
