c174bcd89536b2bd9dcde12a7c77fafe2d1d6853
dtql <3023713788@qq.com>
2025-06-05 10:01:48 +0800

    hot

---
+++ tj-aigc/src/main/java/com/tianji/aigc/service/ChatSessionService.java
@@ -0,0 +1,24 @@
+package com.tianji.aigc.service;
+
+import com.tianji.aigc.vo.SessionVO;
+
+import java.util.List;
+
+public interface ChatSessionService {
+
+    /**
+     * 创建会话session
+     *
+     * @param num 热门问题的数量
+     * @return 会话信息
+     */
+    SessionVO createSession(Integer num);
+
+    /**
+     * 获取热门会话
+     *
+     * @return 热门会话列表
+     */
+    List<SessionVO.Example> hotExamples(Integer num);
+
+}
